fc86b7621528b2165fa161576e2ae4d423c52cfd
Liyan Zhao <return65535@qq.com>
2025-02-12 19:30:50 +0800

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 28
         //noinspection OldTargetApi, my phone is Android 14
         targetSdk = 34
-        versionCode = 40
-        versionName = "0.4"
+        versionCode = 41
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
