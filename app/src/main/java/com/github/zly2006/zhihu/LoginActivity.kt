6e03b3c7364ba0824f8ce2e2dabd2966f73cbbfe
Liyan Zhao <return65535@qq.com>
2026-03-11 14:46:14 +0800

    chore

--- app/src/main/java/com/github/zly2006/zhihu/LoginActivity.kt
+++ app/src/main/java/com/github/zly2006/zhihu/LoginActivity.kt
@@ -20,6 +20,7 @@ import com.github.zly2006.zhihu.ui.PREFERENCE_NAME
 import com.github.zly2006.zhihu.ui.components.WebviewComp
 import com.github.zly2006.zhihu.ui.components.setupUpWebviewClient
 import com.github.zly2006.zhihu.util.enableEdgeToEdgeCompat
+import com.github.zly2006.zhihu.util.luoTianYiUrlLauncher
 import com.github.zly2006.zhihu.util.telemetry
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.delay
@@ -56,11 +57,7 @@ class LoginActivity : ComponentActivity() {
                                 }
                                 if (request.url.host == "graph.qq.com") {
                                     // QQ login
-                                    CustomTabsIntent
-                                        .Builder()
-                                        .setToolbarColor(0xff66CCFF.toInt())
-                                        .build()
-                                        .launchUrl(this@LoginActivity, request.url)
+                                    luoTianYiUrlLauncher(this@LoginActivity, request.url)
                                     return true
                                 }
                                 if (request.url?.scheme == "zhihu") {
