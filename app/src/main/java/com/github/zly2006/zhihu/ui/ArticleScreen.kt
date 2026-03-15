4d7422a2eca61ee8428d4080373a76a46586f05f
ZL114514 <114651468+ZL114514@users.noreply.github.com>
2026-03-15 17:57:05 +0800

    Fix: only add one clipdata
    Updated share text to include URLs for answers and articles.
    
    Some apps like qq and tg won't get mutil ClipData

--- app/src/main/java/com/github/zly2006/zhihu/ui/ArticleScreen.kt
+++ app/src/main/java/com/github/zly2006/zhihu/ui/ArticleScreen.kt
@@ -364,26 +364,16 @@ fun ArticleActionsMenu(
                 onDismissRequest()
                 val text = when (article.type) {
                     ArticleType.Answer -> {
-                        "【${viewModel.title} - ${viewModel.authorName} 的回答】"
+                        "【${viewModel.title} - ${viewModel.authorName} 的回答】- https://www.zhihu.com/question/${viewModel.questionId}/answer/${article.id}"
                     }
 
                     ArticleType.Article -> {
-                        "【${viewModel.title} - ${viewModel.authorName} 的文章】"
-                    }
-                }
-                val uri = when (article.type) {
-                    ArticleType.Answer -> {
-                        "https://www.zhihu.com/question/${viewModel.questionId}/answer/${article.id}"
-                    }
-
-                    ArticleType.Article -> {
-                        "https://zhuanlan.zhihu.com/p/${article.id}"
+                        "【${viewModel.title} - ${viewModel.authorName} 的文章】- https://zhuanlan.zhihu.com/p/${article.id}"
                     }
                 }
                 val shareIntent = Intent().apply {
                     action = Intent.ACTION_SEND
                     type = "text/plain"
-                    putExtra(Intent.EXTRA_STREAM, uri)
                     putExtra(Intent.EXTRA_TEXT, text)
                 }
                 val chooserIntent = Intent.createChooser(shareIntent, "分享到")
