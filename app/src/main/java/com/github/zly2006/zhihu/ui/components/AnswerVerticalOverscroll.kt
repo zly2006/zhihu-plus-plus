96036e77c283350c41a29ad7e57e014b5b568cfb
Liyan Zhao <return65535@qq.com>
2026-03-15 18:47:59 +0800

    fix: vertical overscroll card padding

--- app/src/main/java/com/github/zly2006/zhihu/ui/components/AnswerVerticalOverscroll.kt
+++ app/src/main/java/com/github/zly2006/zhihu/ui/components/AnswerVerticalOverscroll.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -289,7 +292,7 @@ fun AnswerVerticalOverscroll(
                 modifier = Modifier
                     .fillMaxWidth()
                     .align(Alignment.TopCenter)
-                    .padding(top = 8.dp),
+                    .padding(top = WindowInsets.systemBars.asPaddingValues().calculateTopPadding()),
             )
         }
 
@@ -308,7 +311,7 @@ fun AnswerVerticalOverscroll(
                 modifier = Modifier
                     .fillMaxWidth()
                     .align(Alignment.BottomCenter)
-                    .padding(bottom = 8.dp),
+                    .padding(bottom = 8.dp + WindowInsets.systemBars.asPaddingValues().calculateBottomPadding()),
             )
         }
 
