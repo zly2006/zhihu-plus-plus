8318c59e8b2e876ccc4c4859b236bbc1d907101f
Liyan Zhao <return65535@qq.com>
2026-03-20 14:12:20 +0800

    fix: #182

--- app/src/main/java/com/github/zly2006/zhihu/ui/CommentScreen.kt
+++ app/src/main/java/com/github/zly2006/zhihu/ui/CommentScreen.kt
@@ -23,18 +23,16 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -436,7 +434,7 @@ fun CommentScreen(
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(bottom = WindowInsets.systemBars.asPaddingValues().calculateBottomPadding()),
+                    .navigationBarsPadding(),
             ) {
                 CommentTopText(content())
                 Box(modifier = Modifier.weight(1f)) {
