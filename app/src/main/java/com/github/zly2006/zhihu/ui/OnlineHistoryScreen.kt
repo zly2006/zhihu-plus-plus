9303ba1c0d0b4dffbc2ccf3f6d07def186de6c88
Liyan Zhao <return65535@qq.com>
2026-03-27 12:58:58 +0800

    fix: #224

--- app/src/main/java/com/github/zly2006/zhihu/ui/OnlineHistoryScreen.kt
+++ app/src/main/java/com/github/zly2006/zhihu/ui/OnlineHistoryScreen.kt
@@ -2,7 +2,6 @@ package com.github.zly2006.zhihu.ui
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -26,7 +25,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.github.zly2006.zhihu.History
 import com.github.zly2006.zhihu.LocalNavigator
