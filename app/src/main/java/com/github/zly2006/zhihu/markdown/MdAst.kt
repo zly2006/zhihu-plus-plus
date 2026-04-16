6210951526a4b8a1d63fefe113b8b8b8f2a3f405
Liyan Zhao <return65535@qq.com>
2026-04-16 09:43:50 +0800

    fix

--- app/src/main/java/com/github/zly2006/zhihu/markdown/MdAst.kt
+++ app/src/main/java/com/github/zly2006/zhihu/markdown/MdAst.kt
@@ -178,7 +178,7 @@ private fun convertElementToBlock(element: Element): List<MarkdownNode> = when (
     "table" -> listOf(createTableBlock(element))
 
     "div" -> {
-        node.childNodes().convertNodesToBlocks()
+        element.childNodes().convertNodesToBlocks()
     }
 
     "a" -> {
