a57b3c8a7a327a71135cd7f052514629fd72f008
Liyan Zhao <return65535@qq.com>
2026-05-27 06:16:06 +0800

    refactor: 清理本地推荐平台空壳

--- shared/src/androidMain/kotlin/com/github/zly2006/zhihu/viewmodel/local/FeedGenerator.android.kt
+++
@@ -1,6 +0,0 @@
-package com.github.zly2006.zhihu.viewmodel.local
-
-import android.content.Context
-
-fun FeedGenerator(context: Context): FeedGenerator =
-    FeedGenerator(getLocalContentDatabase(context).contentDao())
--- shared/src/androidMain/kotlin/com/github/zly2006/zhihu/viewmodel/local/UserBehaviorAnalyzer.android.kt
+++
@@ -1,6 +0,0 @@
-package com.github.zly2006.zhihu.viewmodel.local
-
-import android.content.Context
-
-fun UserBehaviorAnalyzer(context: Context): UserBehaviorAnalyzer =
-    UserBehaviorAnalyzer(getLocalContentDatabase(context).contentDao())
