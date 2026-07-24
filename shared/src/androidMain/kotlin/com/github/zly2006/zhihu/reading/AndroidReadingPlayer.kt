5dd4aa2e0d6043c4098b624ed350bbf1592de768
Liyan Zhao <return65535@qq.com>
2026-07-24 17:29:45 +0800

    chore

--- shared/src/androidMain/kotlin/com/github/zly2006/zhihu/reading/AndroidReadingPlayer.kt
+++ shared/src/androidMain/kotlin/com/github/zly2006/zhihu/reading/AndroidReadingPlayer.kt
@@ -5,6 +5,14 @@
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation (version 3 only).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.github.zly2006.zhihu.reading
