51b6069269df5fd03e728b36fc0f4d733d1ab01e
Liyan Zhao <return65535@qq.com>
2026-08-16 20:22:21 +0800

    fix: 修复无分组分式参数渲染

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -119,7 +119,7 @@ kotlin {
             implementation("io.ktor:ktor-serialization-kotlinx-json:3.5.0")
             implementation("com.materialkolor:material-kolor:4.1.1")
             implementation("com.fleeksoft.ksoup:ksoup:0.2.6")
-            implementation("io.github.zly2006:latex-renderer:0.0.1-alpha5")
+            implementation("io.github.zly2006:latex-renderer:0.0.1-alpha6")
             implementation(project(":markdown-parser"))
             implementation(project(":markdown-renderer"))
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.8.0")
