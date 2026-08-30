9c4a6c989ae5e34505f29d80f78d88aff7537eb0
Liyan Zhao <return65535@qq.com>
2026-08-31 00:29:15 +0800

    fix: 复制 macOS bottom sheet dialog 宿主

--- shared/src/macosMain/kotlin/com/github/zly2006/zhihu/platform/MacosContentArea.kt
+++ shared/src/macosMain/kotlin/com/github/zly2006/zhihu/platform/MacosContentArea.kt
@@ -1,7 +1,6 @@
 package com.github.zly2006.zhihu.platform
 
 import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 val LocalMacosContentAreaInset = compositionLocalOf { 0.dp }
