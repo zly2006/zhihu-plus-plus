6a8f45b6586e3b3d9c1f36ea76323ea82fd9d928
Liyan Zhao <return65535@qq.com>
2026-08-31 01:10:13 +0800

    fix: 修复 Android 个人主页折叠栏状态栏间距 (#714)

--- shared/src/commonMain/kotlin/com/github/zly2006/zhihu/ui/PeopleScreen.kt
+++ shared/src/commonMain/kotlin/com/github/zly2006/zhihu/ui/PeopleScreen.kt
@@ -29,12 +29,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
@@ -727,7 +730,9 @@ fun PeopleScreen(
             .nestedScroll(scrollBehavior.nestedScrollConnection)
             .fillMaxSize(),
         topBar = {
-            Box {
+            Box(
+                modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars),
+            ) {
                 Column {
                     Box(modifier = Modifier.height(headerHeight)) {
                         UserInfoHeader(
